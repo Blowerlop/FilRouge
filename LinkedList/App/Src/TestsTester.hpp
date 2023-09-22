d1e9378eaa3cda4244e36d1d3e1be27ce94c412f
Blowerlop <Blowerlop@users.noreply.github.com>
2023-09-22 10:37:08 +0200

    Sequence 2

---
+++ LinkedList/App/Src/TestsTester.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+int factorial(int number)
+{
+    // return number <= 1 ? number : factorial(number - 1) * number;  // fail
+    return number <= 1 ? 1 : factorial(number - 1) * number; // pass
+}
